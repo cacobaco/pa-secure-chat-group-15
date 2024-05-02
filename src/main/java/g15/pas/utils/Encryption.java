fa821591cf4f149c5712d29f826ac147ced5ffe4
joaomsh <2079421@student.uma.pt>
2024-05-02 18:05:01 +0100

    feat(Encryption): add Encryption class

---
+++ src/main/java/g15/pas/utils/Encryption.java
@@ -0,0 +1,82 @@
+package g15.pas.utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+/**
+ * This class provides methods for RSA encryption and decryption.
+ */
+public class Encryption {
+
+    /**
+     * Generates a RSA KeyPair.
+     *
+     * @return a RSA KeyPair
+     * @throws Exception if any error occurs during the KeyPair generation
+     */
+    public static KeyPair generateKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    /**
+     * Encrypts a message using RSA encryption.
+     *
+     * @param message   the message to be encrypted
+     * @param publicKey the public key to be used for encryption
+     * @return the encrypted message
+     * @throws Exception if any error occurs during the encryption process
+     */
+    public static byte[] encryptRSA(byte[] message, Key publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(message);
+    }
+
+    /**
+     * Decrypts a message using RSA decryption.
+     *
+     * @param message    the message to be decrypted
+     * @param privateKey the private key to be used for decryption
+     * @return the decrypted message
+     * @throws Exception if any error occurs during the decryption process
+     */
+    public static byte[] decryptRSA(byte[] message, Key privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(message);
+    }
+
+    /**
+     * @param message   the message to be decrypted
+     * @param secretKey the secret key used to decrypt the message
+     * @return the decrypted message as an array of bytes
+     * @throws Exception when the encryption fails
+     */
+    public static byte[] encryptAES(byte[] message, byte[] secretKey) throws Exception {
+        byte[] secretKeyPadded = ByteBuffer.allocate(16).put(secretKey).array();
+        SecretKeySpec secreteKeySpec = new SecretKeySpec(secretKeyPadded, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secreteKeySpec);
+        return cipher.doFinal(message);
+    }
+
+    /**
+     * @param message   the message to be encrypted
+     * @param secretKey the secret key used to encrypt the message
+     * @return the encrypted message as an array of bytes
+     * @throws Exception when the decryption fails
+     */
+    public static byte[] decryptAES(byte[] message, byte[] secretKey) throws Exception {
+        byte[] secretKeyPadded = ByteBuffer.allocate(16).put(secretKey).array();
+        SecretKeySpec secreteKeySpec = new SecretKeySpec(secretKeyPadded, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secreteKeySpec);
+        return cipher.doFinal(message);
+    }
+}
