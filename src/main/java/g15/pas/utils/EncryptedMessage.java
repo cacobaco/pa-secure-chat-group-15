cfd1ab596f2f0b1d3491aed866839c201f521733
joaomsh <2079421@student.uma.pt>
2024-05-02 18:04:14 +0100

    feat(EncryptedMessage): add EncryptedMessage class

---
+++ src/main/java/g15/pas/utils/EncryptedMessage.java
@@ -0,0 +1,29 @@
+package g15.pas.utils;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class EncryptedMessage implements Serializable {
+    private final byte[] encryptedMessage;
+    private final byte[] signature;
+    private final List<String> recipients;
+    private final String sender;
+    public EncryptedMessage(byte[] encryptedMessage, byte[] signature, List<String> recipients, String sender) {
+        this.encryptedMessage = encryptedMessage;
+        this.signature = signature;
+        this.recipients = recipients;
+        this.sender = sender;
+    }
+    public byte[] getEncryptedMessage() {
+        return encryptedMessage;
+    }
+    public byte[] getSignature() {
+        return signature;
+    }
+    public List<String> getRecipients() {
+        return recipients;
+    }
+    public String getSender() {
+        return sender;
+    }
+}
