925efe7fd1ebb1e13dfce5d16020cd5b14acf4ed
cacobaco <lcjmonizf09@gmail.com>
2024-05-12 18:07:46 +0100

    fix: update certificate authority package

--- src/main/java/g15/pas/MainCA.java
+++ src/main/java/g15/pas/MainCA.java
@@ -1,7 +1,7 @@
 package g15.pas;
 
+import g15.pas.ca.CertificateAuthority;
 import g15.pas.exceptions.KeyPairCreationException;
-import g15.pas.server.CertificateAuthority;
 import g15.pas.utils.Config;
 import g15.pas.utils.Logger;
 
